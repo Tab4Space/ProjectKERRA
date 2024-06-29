1965686d779a00de7421f6a4ee21950e37287114
Park Dong Hyeok <skypia0906@gmail.com>
2024-06-29 18:55:46 +0900

    Create player and enemy class

---
+++ Source/Kerra/Public/Character/KerraEnemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/KerraCharacterBase.h"
+#include "KerraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KERRA_API AKerraEnemy : public AKerraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Kerra/Public/Character/KerraPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/KerraCharacterBase.h"
+#include "KerraPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KERRA_API AKerraPlayer : public AKerraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
