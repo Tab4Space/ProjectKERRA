0f1b380dcee5fafcd81e9ec64071e64d2d95ca6e
Park Dong Hyeok <skypia0906@gmail.com>
2024-06-30 13:10:48 +0900

    Create player blueprint

--- Source/Kerra/Private/Character/KerraCharacterBase.cpp
+++ Source/Kerra/Private/Character/KerraCharacterBase.cpp
@@ -3,32 +3,16 @@
 
 #include "Character/KerraCharacterBase.h"
 
-// Sets default values
+
 AKerraCharacterBase::AKerraCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
-// Called when the game starts or when spawned
+
 void AKerraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void AKerraCharacterBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AKerraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- Source/Kerra/Public/Character/KerraCharacterBase.h
+++ Source/Kerra/Public/Character/KerraCharacterBase.h
@@ -12,18 +12,10 @@ class KERRA_API AKerraCharacterBase : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AKerraCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
