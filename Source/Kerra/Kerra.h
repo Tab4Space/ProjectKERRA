e9326d0b915d1ba67de8bc4f593173a379c9ea7c
Park Dong Hyeok <skypia0906@gmail.com>
2024-07-20 15:53:56 +0900

    Add custom log

--- Source/Kerra/Kerra.h
+++ Source/Kerra/Kerra.h
@@ -3,4 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/Engine.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(KERRALOG, Log, All);
+
+#define KERRALOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+#define KERRALOG_S(Verbosity) UE_LOG(KERRALOG, Verbosity, TEXT("%s"), *KERRALOG_CALLINFO)
+#define KERRALOG(Verbosity, Format, ...) UE_LOG(KERRALOG, Verbosity, TEXT("%s %s"), *KERRALOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
