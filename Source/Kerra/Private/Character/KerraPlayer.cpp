7ea1570393517eb241d30a07376da459b6611c9b
Park Dong Hyeok <skypia0906@gmail.com>
2024-08-22 22:36:34 +0900

    Fix character constructure

--- Source/Kerra/Private/Character/KerraPlayer.cpp
+++ Source/Kerra/Private/Character/KerraPlayer.cpp
@@ -4,16 +4,33 @@
 #include "Character/KerraPlayer.h"
 
 #include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Player/KerraPlayerState.h"
 
 AKerraPlayer::AKerraPlayer()
 {
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+	
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("CameraBoom");
 	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->SocketOffset = FVector(0.f, 55.f, 65.f);
+	CameraBoom->bUsePawnControlRotation = true;
 
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>("FollowCamera");
 	FollowCamera->SetupAttachment(CameraBoom);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 500.f, 0.f);
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	
 }
 
