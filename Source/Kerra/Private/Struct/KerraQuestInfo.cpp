f4e396b2f6c2ae5aff99dcc82f480c47c1f8ec31
Park Dong Hyeok <skypia0906@gmail.com>
2024-11-03 22:39:49 +0900

    Add quest info data

---
+++ Source/Kerra/Private/Struct/KerraQuestInfo.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Struct/KerraQuestInfo.h"
+
+bool FKerraQuestInfo::IsValid() const
+{
+	return false;
+}
---
+++ Source/Kerra/Public/Struct/KerraQuestInfo.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameplayTagContainer.h"
+#include "KerraQuestInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FKerraQuestInfo
+{
+	GENERATED_BODY()
+
+	bool IsValid() const;
+};
