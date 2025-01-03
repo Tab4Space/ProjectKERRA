c1b8d2b88ff4110878f294cc4bf6f1d907bcf4e4
Park Dong Hyeok <skypia0906@gmail.com>
2025-01-03 23:21:36 +0900

    Widget sub-classing

---
+++ Source/Kerra/Public/UI/Widget/KerraQuestWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/KerraWidgetBase.h"
+#include "KerraQuestWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KERRA_API UKerraQuestWidget : public UKerraWidgetBase
+{
+	GENERATED_BODY()
+	
+};
